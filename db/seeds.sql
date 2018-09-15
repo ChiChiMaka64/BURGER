31da89c989f09e89633b5bff9c73e7570a3a4082
Lynette Okoroike <lcokoroike@gmail.com>
2018-09-14 21:41:18 -0700

    Currently preparing this recipe.

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO Burgers (burger_name) VALUES ('Double-Double Animal Style');
+INSERT INTO Burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO Burgers (burger_name) VALUES ('Whopper');
+INSERT INTO Burgers (burger_name) VALUES ('Ultimate Cheeseburger');
+INSERT INTO Burgers (burger_name) VALUES ('Hamburger');
