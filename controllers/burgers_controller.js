253b1613535bb6eedfe0dc8adf9c5cc27486c12f
Lynette Okoroike <lcokoroike@gmail.com>
2018-10-26 13:51:34 -0700

    Add a new folder and file.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -12,7 +12,7 @@ router.get("/", function(req, res) {
   });
   router.post("/api/burgers", function(req, res) {
     burger.create([
-      "name", "eaten"
+      'burger_name', "devoured"
     ], [
       req.body.name, req.body.eaten
     ], function(result) {
