9b546a5f9040f5245fb305575e95cf5337802646
Lynette Okoroike <lcokoroike@gmail.com>
2018-11-15 13:34:23 -0800

    I really hate Heroku!

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,11 @@
 var mysql = require("mysql");
-var connection 
-if (process.env.JAWSDB_URL) {
-  connection=mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
+var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "password",
   database: "burgers_db"
 });
-};
 // Make connection.
 connection.connect(function(err) {
   if (err) {
@@ -21,5 +16,5 @@ connection.connect(function(err) {
 });
 
 // Export connection for our ORM to use.
-connection.connect();
+
 module.exports = connection;
