8ab7a1cfeb872b2a0561a3ef6afc1353b7a28a50
Lynette Okoroike <lcokoroike@gmail.com>
2018-11-15 14:34:52 -0800

    made changes to the database.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
-INSERT INTO Burgers (burger_name, devoured) VALUES ('Double-Double Animal Style', 0);
-INSERT INTO Burgers (burger_name, devoured) VALUES ('Big Mac', 0 );
-INSERT INTO Burgers (burger_name, devoured) VALUES ('Whopper', 0);
-INSERT INTO Burgers (burger_name, devoured) VALUES ('Ultimate Cheeseburger', 0);
-INSERT INTO Burgers (burger_name, devoured) VALUES ('Hamburger', 0);
+INSERT INTO Burgers (burger_name, devoured) VALUES ('Double-Double Animal Style', 1);
+INSERT INTO Burgers (burger_name, devoured) VALUES ('Big Mac', 1 );
+INSERT INTO Burgers (burger_name, devoured) VALUES ('Whopper', 1);
+INSERT INTO Burgers (burger_name, devoured) VALUES ('Ultimate Cheeseburger', 1);
+INSERT INTO Burgers (burger_name, devoured) VALUES ('Hamburger', 1);
